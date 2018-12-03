8d3e22ca405ff620762fd8d2355947841291f1ad
Jarred Capellman <jcapellman@hotmail.com>
2018-12-02 18:43:43 -0600

    Added: Initial work on the MainGamePage class

---
+++ src/Whiskey-Tycoon.UWP/Views/MainGamePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+
+using Whiskey_Tycoon.UWP.ViewModels;
+
+namespace Whiskey_Tycoon.UWP.Views
+{
+    public sealed partial class MainGamePage : Page
+    {
+        public MainGamePage()
+        {
+            InitializeComponent();
+
+            DataContext = new MainGamePageViewModel();
+        }
+    }
+}
