0c448363fddc1f614cadab4a111a3ffef358b42a
Jarred Capellman <jcapellman@hotmail.com>
2018-12-02 20:12:03 -0600

    Added: Click actions to the MainPage class

--- src/Whiskey-Tycoon.UWP/MainPage.xaml.cs
+++ src/Whiskey-Tycoon.UWP/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using Whiskey_Tycoon.UWP.Views;
 
 namespace Whiskey_Tycoon.UWP
 {
@@ -7,6 +10,21 @@ namespace Whiskey_Tycoon.UWP
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void btnNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(NewGamePage));
+        }
+
+        private void btnLoadGame_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(LoadGamePage));
+        }
+
+        private void btnOptions_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(OptionsPage));
         }
     }
 }
