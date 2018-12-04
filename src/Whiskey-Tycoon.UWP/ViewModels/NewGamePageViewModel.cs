eaf371d24cdc624dbd225fdd4a05f238d7e60cf0
Jarred Capellman <jcapellman@hotmail.com>
2018-12-03 21:22:21 -0600

    Added: Initial work on the NewGamePageViewModel class

---
+++ src/Whiskey-Tycoon.UWP/ViewModels/NewGamePageViewModel.cs
@@ -0,0 +1,57 @@
+﻿namespace Whiskey_Tycoon.UWP.ViewModels
+{
+    public class NewGamePageViewModel : BaseViewModel
+    {
+        private string _masterDistillerName;
+
+        public string MasterDistillerName
+        {
+            get => _masterDistillerName;
+
+            set
+            {
+                _masterDistillerName = value;
+                OnPropertyChanged();
+
+                UpdateForm();
+            }
+        }
+
+        private string _distilleryName;
+
+        public string DistilleryName
+        {
+            get => _distilleryName;
+
+            set
+            {
+                _distilleryName = value;
+                OnPropertyChanged();
+
+                UpdateForm();
+            }
+        }
+
+        private bool _enableStartGame;
+
+        public bool EnableStartGame
+        {
+            get => _enableStartGame;
+            set
+            {
+                _enableStartGame = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public NewGamePageViewModel()
+        {
+            EnableStartGame = false;
+        }
+
+        private void UpdateForm()
+        {
+            EnableStartGame = !string.IsNullOrEmpty(MasterDistillerName) && !string.IsNullOrEmpty(DistilleryName);
+        }
+    }
+}
