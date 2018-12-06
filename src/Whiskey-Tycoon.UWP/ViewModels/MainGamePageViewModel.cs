46e367229852e33fda9b7f9625860076126eec06
Jarred Capellman <jcapellman@hotmail.com>
2018-12-05 21:33:35 -0600

    Added: GameObject to the MainGamePageViewModel class

--- src/Whiskey-Tycoon.UWP/ViewModels/MainGamePageViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/MainGamePageViewModel.cs
@@ -4,8 +4,21 @@ namespace Whiskey_Tycoon.UWP.ViewModels
 {
     public class MainGamePageViewModel : BaseViewModel
     {
+        private GameObject _gameObject;
+
+        public GameObject Game
+        {
+            get { return _gameObject; }
+            set
+            {
+                _gameObject = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainGamePageViewModel(GameObject gameObject)
         {
+            Game = gameObject;
         }
     }
 }
