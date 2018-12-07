039c486c6ccb954b08b3a37ac589150cca7dc0e0
Jarred Capellman <jcapellman@hotmail.com>
2018-12-06 20:31:46 -0600

    Change: Remove the GameObject from the MainGamePageViewModel class

--- src/Whiskey-Tycoon.UWP/ViewModels/MainGamePageViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/MainGamePageViewModel.cs
@@ -4,18 +4,6 @@ namespace Whiskey_Tycoon.UWP.ViewModels
 {
     public class MainGamePageViewModel : BaseViewModel
     {
-        private GameObject _gameObject;
-
-        public GameObject Game
-        {
-            get { return _gameObject; }
-            set
-            {
-                _gameObject = value;
-                OnPropertyChanged();
-            }
-        }
-
         public MainGamePageViewModel(GameObject gameObject)
         {
             Game = gameObject;
