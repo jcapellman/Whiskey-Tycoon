51f4477f9c4c326dc1ddd9ae0c79502fad3a6a45
Jarred Capellman <jcapellman@hotmail.com>
2018-12-06 20:32:17 -0600

    Added: MoneyAvailable property to the GameObject class

--- src/Whiskey-Tycoon.lib/JSONObjects/GameObject.cs
+++ src/Whiskey-Tycoon.lib/JSONObjects/GameObject.cs
@@ -5,5 +5,8 @@
         public string DistilleryName { get; set; }
 
         public string DistillerName { get; set; }
+
+        // In thousands
+        public int MoneyAvailable { get; set; } = 1000;
     }
 }
