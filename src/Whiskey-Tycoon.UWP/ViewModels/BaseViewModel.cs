28bd12a166e0dd91c376a414aabb7233d48bf29d
Jarred Capellman <jcapellman@hotmail.com>
2018-12-06 20:31:09 -0600

    Change: Added the GameObject to the BaseViewModel class

--- src/Whiskey-Tycoon.UWP/ViewModels/BaseViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/BaseViewModel.cs
@@ -1,10 +1,24 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
+using Whiskey_Tycoon.lib.JSONObjects;
+
 namespace Whiskey_Tycoon.UWP.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
+        private GameObject _gameObject;
+
+        public GameObject Game
+        {
+            get { return _gameObject; }
+            set
+            {
+                _gameObject = value;
+                OnPropertyChanged();
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
