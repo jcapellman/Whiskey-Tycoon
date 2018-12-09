1c0ef4b220c907d8840b74c888f74481b746ae6b
Jarred Capellman <jcapellman@hotmail.com>
2018-12-08 21:24:56 -0600

    Change: Cleaned up the LoadGamePageViewModel class to utilize the IFileSystem class

--- src/Whiskey-Tycoon.UWP/ViewModels/LoadGamePageViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/LoadGamePageViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.ObjectModel;
-
-using Windows.Storage;
-using Windows.Storage.Search;
-
-using Newtonsoft.Json;
+﻿using System.Collections.ObjectModel;
 
 using Whiskey_Tycoon.lib.Common;
 using Whiskey_Tycoon.lib.JSONObjects;
@@ -13,8 +7,6 @@ namespace Whiskey_Tycoon.UWP.ViewModels
 {
     public class LoadGamePageViewModel : BaseViewModel
     {
-        private readonly Windows.Storage.StorageFolder _storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
-
         private ObservableCollection<GameObject> _games;
 
         public ObservableCollection<GameObject> Games {
@@ -31,36 +23,9 @@ namespace Whiskey_Tycoon.UWP.ViewModels
 
         public async void LoadGames()
         {
-            var options = new QueryOptions();
+            var gameList = await App.FileSystem.GetFilesAsync<GameObject>(Constants.FILE_SAVEGAME_EXTENSION);
 
-            options.FileTypeFilter.Add($".{Constants.FILE_SAVEGAME_EXTENSION}");
-            options.FolderDepth = FolderDepth.Deep;
-            
-            var query = _storageFolder.CreateFileQueryWithOptions(options);
-
-            var saveGames = await query.GetFilesAsync();
-
-            foreach (var saveGame in saveGames)
-            {
-                var gameObjectString = await FileIO.ReadTextAsync(saveGame);
-
-                if (string.IsNullOrEmpty(gameObjectString))
-                {
-                    continue;
-                }
-
-                try
-                {
-                    var gameObject = JsonConvert.DeserializeObject<GameObject>(gameObjectString);
-
-                    Games.Add(gameObject);
-                }
-                catch (JsonException)
-                {
-                    // TODO Log
-                    continue;
-                }
-            }
+            Games = new ObservableCollection<GameObject>(gameList);
         }
     }
 }
