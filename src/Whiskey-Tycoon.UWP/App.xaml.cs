e07490d51a3b97f8821bae7c7f15eb7fd76b6aeb
Jarred Capellman <jcapellman@hotmail.com>
2018-12-08 21:23:02 -0600

    Added: IFileSystem class variable in the App class

--- src/Whiskey-Tycoon.UWP/App.xaml.cs
+++ src/Whiskey-Tycoon.UWP/App.xaml.cs
@@ -1,19 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
+using Whiskey_Tycoon.lib.PlatformAbstractions;
+using Whiskey_Tycoon.UWP.PlatformImplementations;
 
 namespace Whiskey_Tycoon.UWP
 {
@@ -22,6 +16,8 @@ namespace Whiskey_Tycoon.UWP
     /// </summary>
     sealed partial class App : Application
     {
+        public static IFileSystem FileSystem = new FileSystem();
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
