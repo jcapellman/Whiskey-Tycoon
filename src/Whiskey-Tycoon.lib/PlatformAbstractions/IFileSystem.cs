6b39f22d00ae4f5da84e49de278db18874e55bab
Jarred Capellman <jcapellman@hotmail.com>
2018-12-08 21:26:06 -0600

    Added: Initial work on the IFileSystem definition

---
+++ src/Whiskey-Tycoon.lib/PlatformAbstractions/IFileSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Whiskey_Tycoon.lib.PlatformAbstractions
+{
+    public interface IFileSystem
+    {
+        bool WriteFile<T>(string fileName, object obj);
+
+        T GetFile<T>(string fileName);
+
+        Task<List<T>> GetFilesAsync<T>(string extension);
+    }
+}
