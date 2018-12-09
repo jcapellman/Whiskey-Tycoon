f09a0c7d0123e1218c733e16b8ff1aff9495e922
Jarred Capellman <jcapellman@hotmail.com>
2018-12-08 18:51:20 -0600

    Added: Games object to the LoadGamePageViewModel class

--- src/Whiskey-Tycoon.UWP/ViewModels/LoadGamePageViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/LoadGamePageViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace Whiskey_Tycoon.UWP.ViewModels
+﻿using System.Collections.Generic;
+
+using Whiskey_Tycoon.lib.JSONObjects;
+
+namespace Whiskey_Tycoon.UWP.ViewModels
 {
     public class LoadGamePageViewModel : BaseViewModel
     {
+        private List<GameObject> _games;
+
+        public List<GameObject> Games {
+            get => _games;
+            set { _games = value; OnPropertyChanged(); }
+        }
     }
 }
