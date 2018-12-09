fb5dfdd59b00714e6e22bbc88b9fc44a6b5967b2
Jarred Capellman <jcapellman@hotmail.com>
2018-12-09 17:22:56 -0600

    Added: Saving game support to the SaveGamePageViewModel class

--- src/Whiskey-Tycoon.UWP/ViewModels/SaveGamePageViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/SaveGamePageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 
 using Whiskey_Tycoon.lib.Common;
@@ -35,8 +36,20 @@ namespace Whiskey_Tycoon.UWP.ViewModels
             var gameList = await App.FileSystem.GetFilesAsync<GameObject>(Constants.FILE_SAVEGAME_EXTENSION);
 
             Games = new ObservableCollection<GameObject>(gameList);
+
+            _currentGame.FileName = Constants.FILE_SAVEGAME_DEFAULT_NAME;
+
+            Games.Insert(0, _currentGame);
         }
 
-        public async Task<bool> SaveGameAsync(string fileName) => await App.FileSystem.WriteFileAsync<GameObject>(fileName, _currentGame);
+        public async Task<bool> SaveGameAsync(string fileName)
+        {
+            if (fileName == Constants.FILE_SAVEGAME_DEFAULT_NAME)
+            {
+                fileName = $"{DateTime.Now.Ticks}.{Constants.FILE_SAVEGAME_EXTENSION}";
+            }
+
+            return await App.FileSystem.WriteFileAsync<GameObject>(fileName, _currentGame);
+        }
     }
 }
