78b1890e26e6798d35251b2b124d0334a0d8cdb4
Jarred Capellman <jcapellman@hotmail.com>
2018-12-08 22:24:08 -0600

    Added: Save functionality to the OptionsPage

--- src/Whiskey-Tycoon.UWP/Views/OptionsPage.xaml.cs
+++ src/Whiskey-Tycoon.UWP/Views/OptionsPage.xaml.cs
@@ -1,18 +1,36 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+
+using Whiskey_Tycoon.UWP.ViewModels;
 
 namespace Whiskey_Tycoon.UWP.Views
 {
     public sealed partial class OptionsPage : Page
     {
+        private OptionsPageViewModel viewModel => (OptionsPageViewModel) DataContext;
+
         public OptionsPage()
         {
             InitializeComponent();
+
+            DataContext = new OptionsPageViewModel();
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
         {
             Frame.GoBack();
+        }
+
+        private async void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await viewModel.SaveOptionsAsync();
+
+            MessageDialog md = new MessageDialog("Options saved");
+
+            await md.ShowAsync();
         }
     }
 }
