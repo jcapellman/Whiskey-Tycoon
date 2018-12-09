18f5508ef5ca02ce1ff0b9246bc03f45f11cd980
Jarred Capellman <jcapellman@hotmail.com>
2018-12-08 22:48:10 -0600

    Added: Initial work on the BasePage class

---
+++ src/Whiskey-Tycoon.UWP/Views/BasePage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+
+namespace Whiskey_Tycoon.UWP.Views
+{
+    public class BasePage : Page
+    {
+        public async void ShowMessage(string content)
+        {
+            var md = new MessageDialog(content);
+
+            await md.ShowAsync();
+        }
+    }
+}
