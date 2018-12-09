5c78b4f955b3ea193f77dfeef3679b1d32ba9e62
Jarred Capellman <jcapellman@hotmail.com>
2018-12-08 22:23:24 -0600

    Added: FILENAME_OPTIONS to the Constants class

--- src/Whiskey-Tycoon.lib/Common/Constants.cs
+++ src/Whiskey-Tycoon.lib/Common/Constants.cs
@@ -2,6 +2,8 @@
 {
     public static class Constants
     {
+        public const string FILENAME_OPTIONS = "options.json";
+
         public const string FILE_SAVEGAME_EXTENSION = "wtsg";
     }
 }
