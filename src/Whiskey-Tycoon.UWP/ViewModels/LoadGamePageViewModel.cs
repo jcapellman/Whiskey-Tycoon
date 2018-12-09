d56ef144ef54083af3e615fdb5811488511a2b4a
Jarred Capellman <jcapellman@hotmail.com>
2018-12-08 19:59:15 -0600

    Added: LoadGames method in the LoadGamePageViewModel class

--- src/Whiskey-Tycoon.UWP/ViewModels/LoadGamePageViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/LoadGamePageViewModel.cs
@@ -1,16 +1,65 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.ObjectModel;
 
+using Windows.Storage;
+using Windows.Storage.Search;
+
+using Newtonsoft.Json;
+
+using Whiskey_Tycoon.lib.Common;
 using Whiskey_Tycoon.lib.JSONObjects;
 
 namespace Whiskey_Tycoon.UWP.ViewModels
 {
     public class LoadGamePageViewModel : BaseViewModel
     {
-        private List<GameObject> _games;
+        private readonly Windows.Storage.StorageFolder _storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
 
-        public List<GameObject> Games {
+        private ObservableCollection<GameObject> _games;
+
+        public ObservableCollection<GameObject> Games {
             get => _games;
             set { _games = value; OnPropertyChanged(); }
+        }
+
+        public LoadGamePageViewModel()
+        {
+            Games = new ObservableCollection<GameObject>();
+
+            LoadGames();
+        }
+
+        public async void LoadGames()
+        {
+            var options = new QueryOptions();
+
+            options.FileTypeFilter.Add($".{Constants.FILE_SAVEGAME_EXTENSION}");
+            options.FolderDepth = FolderDepth.Deep;
+            
+            var query = _storageFolder.CreateFileQueryWithOptions(options);
+
+            var saveGames = await query.GetFilesAsync();
+
+            foreach (var saveGame in saveGames)
+            {
+                var gameObjectString = await FileIO.ReadTextAsync(saveGame);
+
+                if (string.IsNullOrEmpty(gameObjectString))
+                {
+                    continue;
+                }
+
+                try
+                {
+                    var gameObject = JsonConvert.DeserializeObject<GameObject>(gameObjectString);
+
+                    Games.Add(gameObject);
+                }
+                catch (JsonException)
+                {
+                    continue;
+                }
+            }
         }
     }
 }
