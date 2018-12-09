e3f579920001f647d1a21498c04dd9280748c6a0
Jarred Capellman <jcapellman@hotmail.com>
2018-12-08 22:21:12 -0600

    Added: Initial work on the OptionsPageViewModel class

--- src/Whiskey-Tycoon.UWP/ViewModels/OptionsPageViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/OptionsPageViewModel.cs
@@ -1,6 +1,37 @@
-﻿namespace Whiskey_Tycoon.UWP.ViewModels
+﻿using System.Threading.Tasks;
+
+using Whiskey_Tycoon.lib.Common;
+using Whiskey_Tycoon.lib.JSONObjects;
+
+namespace Whiskey_Tycoon.UWP.ViewModels
 {
     public class OptionsPageViewModel : BaseViewModel
     {
+        private OptionsObject _options;
+
+        public OptionsObject Options
+        {
+            get => _options;
+
+            set
+            {
+                _options = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public OptionsPageViewModel()
+        {
+            LoadOptions();
+        }
+
+        private async void LoadOptions()
+        {
+            var optionsResult = await App.FileSystem.GetFileAsync<OptionsObject>(Constants.FILENAME_OPTIONS);
+
+            Options = optionsResult ?? new OptionsObject();
+        }
+
+        public async Task<bool> SaveOptionsAsync() => await App.FileSystem.WriteFileAsync<OptionsObject>(Constants.FILENAME_OPTIONS, Options);
     }
 }
