97721f0d0d59132c92d90fc1b99901f8e00c9a1e
Jarred Capellman <jcapellman@hotmail.com>
2018-12-08 20:15:55 -0600

    Added: GoBack and Navigate functionality to the LoadGamePage class

--- src/Whiskey-Tycoon.UWP/Views/LoadGamePage.xaml.cs
+++ src/Whiskey-Tycoon.UWP/Views/LoadGamePage.xaml.cs
@@ -1,30 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+using Whiskey_Tycoon.UWP.ViewModels;
 
 namespace Whiskey_Tycoon.UWP.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class LoadGamePage : Page
     {
+        private LoadGamePageViewModel viewModel => (LoadGamePageViewModel) DataContext;
+
         public LoadGamePage()
         {
             this.InitializeComponent();
+        }
+
+        private void btnBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+
+        private void btnLoadGame_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainGamePage));
         }
     }
 }
