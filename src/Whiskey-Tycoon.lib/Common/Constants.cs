82aad0e9fbdec3d1c8a5a612e137fe45c3c8a9a5
Jarred Capellman <jcapellman@hotmail.com>
2018-12-09 17:29:23 -0600

    Added: New Save Game Constant

--- src/Whiskey-Tycoon.lib/Common/Constants.cs
+++ src/Whiskey-Tycoon.lib/Common/Constants.cs
@@ -5,5 +5,7 @@
         public const string FILENAME_OPTIONS = "options.json";
 
         public const string FILE_SAVEGAME_EXTENSION = "wtsg";
+
+        public const string FILE_SAVEGAME_DEFAULT_NAME = "New Save Game";
     }
 }
