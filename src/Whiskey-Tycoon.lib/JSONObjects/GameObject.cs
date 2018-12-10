e1b79db763adf3664a9f40e78c170c34ae57637f
Jarred Capellman <jcapellman@hotmail.com>
2018-12-09 22:16:23 -0600

    Added: CurrentDate, CurrentQuarter and CurrentYear properties to the GameObject class

--- src/Whiskey-Tycoon.lib/JSONObjects/GameObject.cs
+++ src/Whiskey-Tycoon.lib/JSONObjects/GameObject.cs
@@ -8,6 +8,12 @@ namespace Whiskey_Tycoon.lib.JSONObjects
 
         public string DistillerName { get; set; }
 
+        public string CurrentDate => $"{CurrentYear}-{CurrentQuarter}";
+
+        public int CurrentYear { get; set; } = DateTime.Now.Year;
+
+        public int CurrentQuarter { get; set; } = 1;
+
         // In thousands
         public int MoneyAvailable { get; set; } = 1000;
 
