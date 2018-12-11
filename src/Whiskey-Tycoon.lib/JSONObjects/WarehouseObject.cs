3e69f3d48032f3920a8d598ae625a50367097feb
Jarred Capellman <jcapellman@hotmail.com>
2018-12-10 21:19:21 -0600

    Added: AgeBarrels method to the WarehouseObject class

--- src/Whiskey-Tycoon.lib/JSONObjects/WarehouseObject.cs
+++ src/Whiskey-Tycoon.lib/JSONObjects/WarehouseObject.cs
@@ -8,6 +8,14 @@ namespace Whiskey_Tycoon.lib.JSONObjects
 
         public List<BarrelObject> Barrels { get; set; }
 
+        public void AgeBarrels()
+        {
+            foreach (var barrel in Barrels)
+            {
+                barrel.AgeBarrel();
+            }
+        }
+
         public WarehouseObject()
         {
             Barrels = new List<BarrelObject>();
