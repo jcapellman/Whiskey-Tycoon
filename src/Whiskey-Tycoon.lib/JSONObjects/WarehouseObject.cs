24e2e92ba3ed51657cd6b3558a4cef7a9627f504
Jarred Capellman <jcapellman@hotmail.com>
2018-12-10 20:39:22 -0600

    Added: Initial work on the WarehouseObject class

---
+++ src/Whiskey-Tycoon.lib/JSONObjects/WarehouseObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Whiskey_Tycoon.lib.JSONObjects
+{
+    public class WarehouseObject
+    {
+        public string Name { get; set; }
+
+        public List<BarrelObject> Barrels { get; set; }
+
+        public WarehouseObject()
+        {
+            Barrels = new List<BarrelObject>();
+        }
+    }
+}
