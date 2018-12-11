71f259350c7f5442bdec7b57951b59082a6430a5
Jarred Capellman <jcapellman@hotmail.com>
2018-12-10 21:12:43 -0600

    Added: Initial work on the EventObject class

---
+++ src/Whiskey-Tycoon.lib/JSONObjects/EventObject.cs
@@ -0,0 +1,11 @@
+﻿namespace Whiskey_Tycoon.lib.JSONObjects
+{
+    public class EventObject
+    {
+        public int Year { get; set; }
+
+        public int Quarter { get; set; }
+
+        public string EventText { get; set; }
+    }
+}
