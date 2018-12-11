b9de4f624f4f539d79e99a133b03ff27fba64fe6
Jarred Capellman <jcapellman@hotmail.com>
2018-12-10 20:39:02 -0600

    Added: Initial work on the BarrelObject class

---
+++ src/Whiskey-Tycoon.lib/JSONObjects/BarrelObject.cs
@@ -0,0 +1,13 @@
+﻿namespace Whiskey_Tycoon.lib.JSONObjects
+{
+    public class BarrelObject
+    {
+        public int ID { get; set; }
+
+        public int BarrelYear { get; set; }
+
+        public int BarrelQuarter { get; set; }
+
+        public float FillAmount { get; set; }
+    }
+}
