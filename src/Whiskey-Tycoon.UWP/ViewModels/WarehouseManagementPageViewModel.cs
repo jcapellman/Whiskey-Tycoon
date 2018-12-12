266ce5cef028fd4969a7d25ec5197924c5706983
Jarred Capellman <jcapellman@hotmail.com>
2018-12-11 22:06:00 -0600

    Added: WarehouseSizes Enum and ExtensionMethod

--- src/Whiskey-Tycoon.UWP/ViewModels/WarehouseManagementPageViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/WarehouseManagementPageViewModel.cs
@@ -1,12 +1,82 @@
-﻿using Whiskey_Tycoon.lib.JSONObjects;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Whiskey_Tycoon.lib.Enums;
+using Whiskey_Tycoon.lib.JSONObjects;
 
 namespace Whiskey_Tycoon.UWP.ViewModels
 {
     public class WarehouseManagementPageViewModel : BaseViewModel
     {
+        private int _warehouseCost;
+
+        public int WarehouseCost
+        {
+            get => _warehouseCost;
+
+            set
+            {
+                _warehouseCost = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<string> _warehouseSizes;
+
+        public List<string> WarehouseSizes
+        {
+            get => _warehouseSizes;
+
+            set
+            {
+                _warehouseSizes = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _selectedWarehouseSize;
+
+        public string SelectedWarehouseSize
+        {
+            get => _selectedWarehouseSize;
+
+            set
+            {
+                _selectedWarehouseSize = value;
+                OnPropertyChanged();
+
+                WarehouseCost = ((WarehouseSizes) Enum.Parse(typeof(WarehouseSizes), value)).ToCost();
+            }
+        }
+
+        private string _warehouseName;
+
+        public string WarehouseName
+        {
+            get => _warehouseName;
+
+            set
+            {
+                _warehouseName = value;
+
+                OnPropertyChanged();
+            }
+        }
+
         public WarehouseManagementPageViewModel(GameObject game)
         {
             Game = game;
+
+            WarehouseSizes = Enum.GetNames(typeof(WarehouseSizes)).ToList();
+
+            ClearForm();
+        }
+
+        public void ClearForm()
+        {
+            WarehouseName = string.Empty;
+            SelectedWarehouseSize = WarehouseSizes.FirstOrDefault();
         }
     }
 }
---
+++ src/Whiskey-Tycoon.lib/Enums/WarehouseSizes.cs
@@ -0,0 +1,33 @@
+﻿namespace Whiskey_Tycoon.lib.Enums
+{
+    public enum WarehouseSizes
+    {
+        Tiny = 100,
+        Small = 1000,
+        Medium = 5000,
+        Large = 10000,
+        ExtraLarge = 50000
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int ToCost(this WarehouseSizes warehouseSize)
+        {
+            switch (warehouseSize)
+            {
+                case WarehouseSizes.Tiny:
+                    return 10000;
+                case WarehouseSizes.Small:
+                    return 20000;
+                case WarehouseSizes.Medium:
+                    return 50000;
+                case WarehouseSizes.Large:
+                    return 100000;
+                case WarehouseSizes.ExtraLarge:
+                    return 500000;
+            }
+
+            return -1;
+        }
+    }
+}
