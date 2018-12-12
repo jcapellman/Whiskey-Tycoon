1edb7664be32a5d068b7484fc3d08b73465f85d7
Jarred Capellman <jcapellman@hotmail.com>
2018-12-11 20:42:01 -0600

    Added: Initial work on the WarehouseManagementPage

---
+++ src/Whiskey-Tycoon.UWP/Views/WarehouseManagementPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+using Whiskey_Tycoon.lib.JSONObjects;
+using Whiskey_Tycoon.UWP.ViewModels;
+
+namespace Whiskey_Tycoon.UWP.Views
+{
+    public sealed partial class WarehouseManagementPage : Page
+    {
+        public WarehouseManagementPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (!(e.Parameter is GameObject))
+            {
+                return;
+            }
+
+            DataContext = new WarehouseManagementPageViewModel((GameObject)e.Parameter);
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+    }
+}
