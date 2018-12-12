74a4c90aced3cbfdaf0345f2392129ea1359c9a7
Jarred Capellman <jcapellman@hotmail.com>
2018-12-11 20:42:28 -0600

    Added: Initial work on the WarehouseManagementPageViewModel class

---
+++ src/Whiskey-Tycoon.UWP/ViewModels/WarehouseManagementPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Whiskey_Tycoon.lib.JSONObjects;
+
+namespace Whiskey_Tycoon.UWP.ViewModels
+{
+    public class WarehouseManagementPageViewModel : BaseViewModel
+    {
+        public WarehouseManagementPageViewModel(GameObject game)
+        {
+            Game = game;
+        }
+    }
+}
