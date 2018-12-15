93f379037c81c39da0b9fc7108910e7cc40096c5
Jarred Capellman <jcapellman@hotmail.com>
2018-12-15 11:28:23 -0600

    Added: Initial work on the WarehousePageViewModel class

---
+++ src/Whiskey-Tycoon.UWP/ViewModels/WarehousePageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Whiskey_Tycoon.lib.Containers;
+using Whiskey_Tycoon.lib.JSONObjects;
+
+namespace Whiskey_Tycoon.UWP.ViewModels
+{
+    public class WarehousePageViewModel : BaseViewModel
+    {
+        private string _warehouseHeaderName;
+
+        public string WarehouseHeaderName
+        {
+            get => _warehouseHeaderName;
+
+            set
+            {
+                _warehouseHeaderName = $"Manage Warehouse {value}";
+
+                OnPropertyChanged();
+            }
+        }
+
+        private WarehouseObject _warehouse;
+
+        public WarehouseObject Warehouse
+        {
+            get => _warehouse;
+            set
+            {
+                _warehouse = value;
+                OnPropertyChanged();
+
+                WarehouseHeaderName = value.Name;
+            }
+        }
+
+        public WarehousePageViewModel(ManageWarehouseContainer container)
+        {
+            Game = container.Game;
+            Warehouse = container.SelectedWarehouse;
+        }
+    }
+}
