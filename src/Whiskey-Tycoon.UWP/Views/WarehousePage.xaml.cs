574d8071b7faf836520e57fc9a34a6b499e4bc5e
Jarred Capellman <jcapellman@hotmail.com>
2018-12-15 11:39:14 -0600

    Added: Initial work on the WarehousePage class

---
+++ src/Whiskey-Tycoon.UWP/Views/WarehousePage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Whiskey_Tycoon.lib.Containers;
+using Whiskey_Tycoon.lib.JSONObjects;
+using Whiskey_Tycoon.UWP.ViewModels;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Whiskey_Tycoon.UWP.Views
+{
+    public sealed partial class WarehousePage : BasePage
+    {
+        private WarehousePageViewModel ViewModel => (WarehousePageViewModel) DataContext;
+
+        public WarehousePage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (!(e.Parameter is ManageWarehouseContainer))
+            {
+                return;
+            }
+
+            DataContext = new WarehousePageViewModel((ManageWarehouseContainer)e.Parameter);
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+    }
+}
