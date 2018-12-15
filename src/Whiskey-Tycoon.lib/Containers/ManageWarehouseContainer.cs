df68ec11d44c24cc8a5d6a55d628283765b9bf64
Jarred Capellman <jcapellman@hotmail.com>
2018-12-15 11:39:47 -0600

    Added: ManageWarehouseContainer class

---
+++ src/Whiskey-Tycoon.lib/Containers/ManageWarehouseContainer.cs
@@ -0,0 +1,11 @@
+﻿using Whiskey_Tycoon.lib.JSONObjects;
+
+namespace Whiskey_Tycoon.lib.Containers
+{
+    public class ManageWarehouseContainer
+    {
+        public GameObject Game { get; set; }
+
+        public WarehouseObject SelectedWarehouse { get; set; }
+    }
+}
