063e554a356fc5032c4c2935e9a38c0f0c9d4f53
Jarred Capellman <jcapellman@hotmail.com>
2018-12-16 21:38:33 -0600

    Added: Initial form logic in the WarehousePageViewModel class

--- src/Whiskey-Tycoon.UWP/ViewModels/WarehousePageViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/WarehousePageViewModel.cs
@@ -1,4 +1,9 @@
-﻿using Whiskey_Tycoon.lib.Containers;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+using Whiskey_Tycoon.lib.Containers;
+using Whiskey_Tycoon.lib.Enums;
 using Whiskey_Tycoon.lib.JSONObjects;
 
 namespace Whiskey_Tycoon.UWP.ViewModels
@@ -33,10 +38,113 @@ namespace Whiskey_Tycoon.UWP.ViewModels
             }
         }
 
+        private ObservableCollection<string> _availableBatchTypes;
+
+        public ObservableCollection<string> AvailableBatchTypes
+        {
+            get => _availableBatchTypes;
+            set
+            {
+                _availableBatchTypes = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _selectedBatchType;
+
+        public string SelectedBatchType
+        {
+            get => _selectedBatchType;
+
+            set
+            {
+                _selectedBatchType = value;
+                OnPropertyChanged();
+                
+                ValidateForm();
+            }
+        }
+
+        private int _numberBarrels;
+
+        public int NumberBarrels
+        {
+            get => _numberBarrels;
+
+            set
+            {
+                _numberBarrels = value;
+                OnPropertyChanged();
+
+                ValidateForm();
+            }
+        }
+
+        private ulong _batchCost;
+
+        public ulong BatchCost
+        {
+            get => _batchCost;
+
+            set
+            {
+                _batchCost = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _batchName;
+
+        public string BatchName
+        {
+            get => _batchName;
+
+            set
+            {
+                _batchName = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _enableCreateButton;
+
+        public bool EnableCreateButton
+        {
+            get => _enableCreateButton;
+            set
+            {
+                _enableCreateButton = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void ValidateForm()
+        {
+            if (NumberBarrels > Warehouse.SpaceAvailable)
+            {
+                NumberBarrels = Warehouse.SpaceAvailable;
+            }
+
+            BatchCost = (ulong) ((int)(BatchTypes)Enum.Parse(typeof(BatchTypes), SelectedBatchType) * NumberBarrels);
+
+            EnableCreateButton = !string.IsNullOrEmpty(BatchName) && BatchCost <= Game.MoneyAvailable;
+        }
+
+        private void ClearForm()
+        {
+            BatchName = string.Empty;
+            SelectedBatchType = AvailableBatchTypes.FirstOrDefault();
+            NumberBarrels = 0;
+        }
+
         public WarehousePageViewModel(ManageWarehouseContainer container)
         {
             Game = container.Game;
             Warehouse = container.SelectedWarehouse;
+            AvailableBatchTypes = new ObservableCollection<string>(Enum.GetNames(typeof(BatchTypes)));
+            
+            ClearForm();
         }
     }
 }
