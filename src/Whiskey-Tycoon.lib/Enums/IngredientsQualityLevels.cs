fbc3e2daebf472bf74f6d52d8cfc774f17bafda8
Jarred Capellman <jcapellman@hotmail.com>
2018-12-17 21:32:51 -0600

    Added: IngredientsQualityLevels Enum

---
+++ src/Whiskey-Tycoon.lib/Enums/IngredientsQualityLevels.cs
@@ -0,0 +1,16 @@
+﻿namespace Whiskey_Tycoon.lib.Enums
+{
+    public enum IngredientsQualityLevels
+    {
+        CHEAP = 100,
+        POOR = 110,
+        MID = 120,
+        HIGH = 130,
+        BEST = 140
+    }
+
+    public static class IngredientsExtensions
+    {
+        public static float ToQualityLevelMultiplier(this IngredientsQualityLevels qualityLevel) => ((int)qualityLevel / 100.0f);
+    }
+}
