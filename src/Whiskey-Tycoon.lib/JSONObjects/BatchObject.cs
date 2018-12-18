1c235dc1e571753e2b337a700326ead5cce2ccb5
Jarred Capellman <jcapellman@hotmail.com>
2018-12-17 21:33:29 -0600

    Added: Initial work on the BatchObject class

---
+++ src/Whiskey-Tycoon.lib/JSONObjects/BatchObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+using Whiskey_Tycoon.lib.Enums;
+
+namespace Whiskey_Tycoon.lib.JSONObjects
+{
+    public class BatchObject
+    {
+        public string Name { get; set; }
+
+        public int BarrelQuarter { get; set; }
+
+        public int BarrelYear { get; set; }
+
+        public int BarrelQuarterAge { get; set; }
+
+        public IngredientsQualityLevels QualityLevel { get; set; }
+
+        public BatchTypes BatchType { get; set; }
+
+        public List<BarrelObject> Barrels { get; set; }
+
+        public BatchObject()
+        {
+            Barrels = new List<BarrelObject>();
+        }
+    }
+}
