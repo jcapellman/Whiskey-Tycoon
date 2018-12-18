f154134120a69bd1959528a1d13dcf22ec0cd792
Jarred Capellman <jcapellman@hotmail.com>
2018-12-17 22:42:59 -0600

    Added: Extension Methods for the IngredientsQualityLevels enum

--- src/Whiskey-Tycoon.lib/Enums/IngredientsQualityLevels.cs
+++ src/Whiskey-Tycoon.lib/Enums/IngredientsQualityLevels.cs
@@ -12,5 +12,43 @@
     public static class IngredientsExtensions
     {
         public static float ToQualityLevelMultiplier(this IngredientsQualityLevels qualityLevel) => ((int)qualityLevel / 100.0f);
+
+        public static int ToQuality(this IngredientsQualityLevels qualityLevel)
+        {
+            switch (qualityLevel)
+            {
+                case IngredientsQualityLevels.CHEAP:
+                    return 0;
+                case IngredientsQualityLevels.POOR:
+                    return 5;
+                case IngredientsQualityLevels.MID:
+                    return 10;
+                case IngredientsQualityLevels.HIGH:
+                    return 15;
+                case IngredientsQualityLevels.BEST:
+                    return 20;
+            }
+
+            return 0;
+        }
+
+        public static int ToQualityQuarterIncrement(this IngredientsQualityLevels qualityLevel)
+        {
+            switch (qualityLevel)
+            {
+                case IngredientsQualityLevels.CHEAP:
+                    return 1;
+                case IngredientsQualityLevels.POOR:
+                    return 2;
+                case IngredientsQualityLevels.MID:
+                    return 3;
+                case IngredientsQualityLevels.HIGH:
+                    return 4;
+                case IngredientsQualityLevels.BEST:
+                    return 5;
+            }
+
+            return 1;
+        }
     }
 }
