0e517c775b626eae3d2801c2ceeeec44c3e369fd
Jarred Capellman <jcapellman@hotmail.com>
2018-12-18 21:07:28 -0600

    Added: Initial work on the ReleaseBatchPageViewModel class

---
+++ src/Whiskey-Tycoon.UWP/ViewModels/ReleaseBatchPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Whiskey_Tycoon.lib.Containers;
+using Whiskey_Tycoon.lib.JSONObjects;
+
+namespace Whiskey_Tycoon.UWP.ViewModels
+{
+    public class ReleaseBatchPageViewModel : BaseViewModel
+    {
+        private WarehouseObject _warehouseObject;
+
+        private BatchObject _batchObject;
+
+        public ManageWarehouseContainer Container => new ManageWarehouseContainer
+        {
+            Game = Game,
+            SelectedWarehouse = _warehouseObject,
+            SelectedBatch = _batchObject
+        };
+
+        public ReleaseBatchPageViewModel(ManageWarehouseContainer container)
+        {
+            Game = container.Game;
+            _warehouseObject = container.SelectedWarehouse;
+            _batchObject = container.SelectedBatch;
+        }
+    }
+}
