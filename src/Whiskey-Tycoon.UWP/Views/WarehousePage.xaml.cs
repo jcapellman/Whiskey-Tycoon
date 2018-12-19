5c67dbb1ff623751cd58b8b9eb6c5c121224e78c
Jarred Capellman <jcapellman@hotmail.com>
2018-12-18 20:47:30 -0600

    Added: btnTrash_Click implementation in the WarehousePage class

--- src/Whiskey-Tycoon.UWP/Views/WarehousePage.xaml.cs
+++ src/Whiskey-Tycoon.UWP/Views/WarehousePage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using Whiskey_Tycoon.lib.Containers;
-using Whiskey_Tycoon.UWP.ViewModels;
-
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+
+using Whiskey_Tycoon.lib.Containers;
+using Whiskey_Tycoon.UWP.ViewModels;
+using Whiskey_Tycoon.lib.JSONObjects;
 
 namespace Whiskey_Tycoon.UWP.Views
 {
@@ -35,6 +37,27 @@ namespace Whiskey_Tycoon.UWP.Views
         private void btnCreateBatch_Click(object sender, RoutedEventArgs e)
         {
             ViewModel.AddBatch();
+        }
+
+        private async void btnTrash_Click(object sender, RoutedEventArgs e)
+        {
+            var dialogResult = await ShowYesNoDialogAsync("Are you sure you want to trash this batch?");
+
+            if (!dialogResult)
+            {
+                return;
+            }
+
+            var button = (Button)e.OriginalSource;
+
+            var batchObject = (BatchObject)button.DataContext;
+
+            var deletionResult = ViewModel.DeleteBatch(batchObject);
+
+            if (!deletionResult)
+            {
+                ShowMessage($"{batchObject.Name} could not be trashed");
+            }
         }
     }
 }
