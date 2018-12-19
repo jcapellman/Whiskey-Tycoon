f39e727a041188ee695982af746cb4f6fd3b1fd3
Jarred Capellman <jcapellman@hotmail.com>
2018-12-18 21:53:38 -0600

    Added: ReleaseTheBatch method to the ReleaseBatchPageViewModel class

--- src/Whiskey-Tycoon.UWP/ViewModels/ReleaseBatchPageViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/ReleaseBatchPageViewModel.cs
@@ -20,7 +20,20 @@ namespace Whiskey_Tycoon.UWP.ViewModels
                 OnPropertyChanged();
             }
         }
-        
+
+        private int _price;
+
+        public int Price
+        {
+            get => _price;
+
+            set
+            {
+                _price = value;
+                OnPropertyChanged();
+            }
+        }
+
         public string ReleaseBatchName => $"Release {Batch.Name}";
 
         public ManageWarehouseContainer Container => new ManageWarehouseContainer
@@ -35,6 +48,22 @@ namespace Whiskey_Tycoon.UWP.ViewModels
             Game = container.Game;
             _warehouseObject = container.SelectedWarehouse;
             _batchObject = container.SelectedBatch;
+        }
+
+        public void ReleaseTheBatch()
+        {
+            var releaseObject = new ReleasesObject
+            {
+                Name = Batch.Name,
+                BottlesSold = 0,
+                BottlePrice = Price,
+                QualityRating = Batch.Quality,
+                ReleaseQuarter = Game.CurrentQuarter,
+                ReleaseYear = Game.CurrentYear,
+                YearsAged = Batch.BarrelQuarterAge / 4.0f
+            };
+
+            Game.Releases.Add(releaseObject);
         }
     }
 }
