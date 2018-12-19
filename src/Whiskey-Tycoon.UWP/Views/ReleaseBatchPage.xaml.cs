d2b782d384faf3c0d475985d050628dd9ca82cdb
Jarred Capellman <jcapellman@hotmail.com>
2018-12-18 21:08:19 -0600

    Added: Initial work on the ReleaseBatchPage class

---
+++ src/Whiskey-Tycoon.UWP/Views/ReleaseBatchPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
+
+using Whiskey_Tycoon.lib.Containers;
+using Whiskey_Tycoon.UWP.ViewModels;
+
+namespace Whiskey_Tycoon.UWP.Views
+{
+    public sealed partial class ReleaseBatchPage : BasePage
+    {
+        private ReleaseBatchPageViewModel ViewModel => (ReleaseBatchPageViewModel) DataContext;
+
+        public ReleaseBatchPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (!(e.Parameter is ManageWarehouseContainer))
+            {
+                return;
+            }
+
+            DataContext = new ReleaseBatchPageViewModel((ManageWarehouseContainer)e.Parameter);
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(WarehousePage), ViewModel.Container);
+        }
+    }
+}
