4b58ebf98ff8c4bac4780d31dc4fde3e0f515030
Jarred Capellman <jcapellman@hotmail.com>
2018-12-23 13:33:33 -0600

    Added: Initial commit of the AngelsShare class

---
+++ src/Whiskey-Tycoon.lib/Helpers/AngelsShare.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Whiskey_Tycoon.lib.Helpers
+{
+    public static class AngelsShare
+    {
+        private const int SHARE_MIN = 1;
+        private const int SHARE_MAX = 4;
+
+        public static int ComputeAngelShare(int fillAmount)
+        {
+            var random = new Random((int)DateTime.Now.Ticks);
+            
+            return fillAmount - random.Next(SHARE_MIN, SHARE_MAX);
+        } 
+    }
+}
