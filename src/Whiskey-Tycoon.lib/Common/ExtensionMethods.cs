4ff8f04ed7d66bf5dc7faeb9a087148ef0b31d26
Jarred Capellman <jcapellman@hotmail.com>
2018-12-24 15:47:34 -0500

    Added: GetRandomItem extension method

---
+++ src/Whiskey-Tycoon.lib/Common/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Whiskey_Tycoon.lib.Common
+{
+    public static class ExtensionMethods
+    {
+        public static T GetRandomItem<T>(this ObservableCollection<T> container)
+        {
+            if (!container.Any())
+            {
+                return default;
+            }
+
+            var random = new Random((int)DateTime.Now.Ticks);
+
+            var index = random.Next(0, container.Count - 1);
+
+            return container[index];
+        }
+    }
+}
