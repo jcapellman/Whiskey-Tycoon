d21503bc09300e7b6e40dfc3ea589ded23642fa9
Jarred Capellman <jcapellman@hotmail.com>
2018-12-28 08:22:02 -0800

    Added: Initial work on the PressSampleReviewObject class

---
+++ src/Whiskey-Tycoon.lib/JSONObjects/PressSampleReviewObject.cs
@@ -0,0 +1,13 @@
+﻿namespace Whiskey_Tycoon.lib.JSONObjects
+{
+    public class PressSampleReviewObject
+    {
+        public int Quarter { get; set; }
+
+        public int Year { get; set; }
+
+        public string Review { get; set; }
+
+        public bool Positive { get; set; }
+    }
+}
