1f0263f2363418b4cd741a84cf921a1c7355801d
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:04:14 -0800

    Added: Flushed out the LoanManagementPageViewModel class #13

--- src/Whiskey-Tycoon.UWP/ViewModels/LoanManagementPageViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/LoanManagementPageViewModel.cs
@@ -1,17 +1,54 @@
-﻿using Whiskey_Tycoon.lib.JSONObjects;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+using Whiskey_Tycoon.lib.JSONObjects;
+using Whiskey_Tycoon.lib.Loans.Base;
+using Whiskey_Tycoon.lib.Managers;
 
 namespace Whiskey_Tycoon.UWP.ViewModels
 {
     public class LoanManagementPageViewModel : BaseViewModel
     {
+        private ObservableCollection<BaseLoan> _loanTypes;
+
+        public ObservableCollection<BaseLoan> LoanTypes
+        {
+            get => _loanTypes;
+
+            set
+            {
+                _loanTypes = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private BaseLoan _selectedLoan;
+
+        public BaseLoan SelectedLoan
+        {
+            get => _selectedLoan;
+
+            set
+            {
+                _selectedLoan = value;
+
+                OnPropertyChanged();
+            }
+        }
+
         public LoanManagementPageViewModel(GameObject game)
         {
             Game = game;
+
+            LoanTypes = LoanManager.GetLoanTypes();
+
+            SelectedLoan = LoanTypes.FirstOrDefault();
         }
 
-        public void AddLoan(ulong amount, ulong quarterlyInterest, uint quarterInstallments)
+        public void AddLoan()
         {
-            Game.AddLoan(amount, quarterlyInterest, quarterInstallments);
+            Game.AddLoan(SelectedLoan);
         }
     }
 }
