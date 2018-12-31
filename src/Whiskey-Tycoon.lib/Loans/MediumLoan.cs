3167ddbeb3f6caadbeceb4c4b26a11691aea36a1
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:03:03 -0800

    Added: Initial work on the MediumLoan class #13

---
+++ src/Whiskey-Tycoon.lib/Loans/MediumLoan.cs
@@ -0,0 +1,15 @@
+﻿using Whiskey_Tycoon.lib.Loans.Base;
+
+namespace Whiskey_Tycoon.lib.Loans
+{
+    public class MediumLoan : BaseLoan
+    {
+        public override string Name => "Medium Loan";
+
+        public override ulong Amount => 500000;
+
+        public override uint QuartersToPayOff => 16;
+
+        public override ulong QuarterlyInterest => 75000;
+    }
+}
