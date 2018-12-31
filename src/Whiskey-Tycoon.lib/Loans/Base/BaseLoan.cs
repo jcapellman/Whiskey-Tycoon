fcbf3acfe0b6c34ea16010172e0a4133ca4c9f1e
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:01:43 -0800

    Added: Initial work on the BaseLoan class #13

---
+++ src/Whiskey-Tycoon.lib/Loans/Base/BaseLoan.cs
@@ -0,0 +1,13 @@
+﻿namespace Whiskey_Tycoon.lib.Loans.Base
+{
+    public abstract class BaseLoan
+    {
+        public abstract string Name { get; }
+
+        public abstract ulong Amount { get; }
+
+        public abstract uint QuartersToPayOff { get; }
+
+        public abstract ulong QuarterlyInterest { get; }
+    }
+}
