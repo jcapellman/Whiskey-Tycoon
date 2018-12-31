8bb1b28e34773d91af154b0a438b727f5f551bc4
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:27:38 -0800

    Added: Initial work on the MarketingManagementPageViewModel class #9

---
+++ src/Whiskey-Tycoon.UWP/ViewModels/MarketingManagementPageViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+using Whiskey_Tycoon.lib.JSONObjects;
+using Whiskey_Tycoon.lib.Loans.Base;
+using Whiskey_Tycoon.lib.Managers;
+
+namespace Whiskey_Tycoon.UWP.ViewModels
+{
+    public class MarketingManagementPageViewModel : BaseViewModel
+    {
+        private ObservableCollection<BaseLoan> _marketingTypes;
+
+        public ObservableCollection<BaseLoan> MarketingTypes
+        {
+            get => _marketingTypes;
+
+            set
+            {
+                _marketingTypes = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _enableBuyMarketingButton;
+
+        public bool EnableBuyMarketingButton
+        {
+            get => _enableBuyMarketingButton;
+
+            set
+            {
+                _enableBuyMarketingButton = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private BaseLoan _selectedMarketing;
+
+        public BaseLoan SelectedMarketing
+        {
+            get => _selectedMarketing;
+
+            set
+            {
+                _selectedMarketing = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public MarketingManagementPageViewModel(GameObject game)
+        {
+            Game = game;
+
+            MarketingTypes = LoanManager.GetLoanTypes();
+
+            SelectedMarketing = MarketingTypes.FirstOrDefault();
+
+            EnableBuyMarketingButton = !Game.Loans.Any();
+        }
+
+        public void AddMarketing()
+        {
+            Game.AddMarketing(SelectedMarketing);
+        }
+    }
+}
