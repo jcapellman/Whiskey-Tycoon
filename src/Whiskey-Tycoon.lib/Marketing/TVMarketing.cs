fa395f07acbfe3c5120fb08a559a30bdf85db9a9
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:52:33 -0800

    Added: Initial work on the TVMarketing class #9

---
+++ src/Whiskey-Tycoon.lib/Marketing/TVMarketing.cs
@@ -0,0 +1,15 @@
+﻿using Whiskey_Tycoon.lib.Marketing.Base;
+
+namespace Whiskey_Tycoon.lib.Marketing
+{
+    public class TVMarketing : BaseMarketing
+    {
+        public override string Name => "TV";
+
+        public override uint Impact => 10;
+
+        public override uint QuartersLength => 4;
+
+        public override ulong QuarterCost => 30000;
+    }
+}
