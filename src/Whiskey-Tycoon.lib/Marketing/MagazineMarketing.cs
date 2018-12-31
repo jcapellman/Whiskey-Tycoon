17bd912da61ede2a4e6bf43342b8e6f54754ca0f
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:51:53 -0800

    Added: Initial work on the MagazineMarketing class #9

---
+++ src/Whiskey-Tycoon.lib/Marketing/MagazineMarketing.cs
@@ -0,0 +1,15 @@
+﻿using Whiskey_Tycoon.lib.Marketing.Base;
+
+namespace Whiskey_Tycoon.lib.Marketing
+{
+    public class MagazineMarketing : BaseMarketing
+    {
+        public override string Name => "Magazine";
+
+        public override uint Impact => 5;
+
+        public override uint QuartersLength => 4;
+
+        public override ulong QuarterCost => 10000;
+    }
+}
