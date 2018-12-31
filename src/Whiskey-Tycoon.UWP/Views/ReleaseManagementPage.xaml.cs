6d40718f23d852d6646d061abd4c4efcaa2dc5b8
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 08:17:51 -0800

    Added: Initial work on the ReleaseManagementPage view

---
+++ src/Whiskey-Tycoon.UWP/Views/ReleaseManagementPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
+
+using Whiskey_Tycoon.lib.JSONObjects;
+using Whiskey_Tycoon.UWP.ViewModels;
+
+namespace Whiskey_Tycoon.UWP.Views
+{
+    public sealed partial class ReleaseManagementPage : BasePage
+    {
+        private ReleaseManagementPageViewModel ViewModel => (ReleaseManagementPageViewModel) DataContext;
+
+        public ReleaseManagementPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (!(e.Parameter is GameObject))
+            {
+                return;
+            }
+
+            DataContext = new ReleaseManagementPageViewModel((GameObject)e.Parameter);
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainGamePage), ViewModel.Game);
+        }
+    }
+}
