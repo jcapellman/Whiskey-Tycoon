a061c7415215d66af9ec08dc5d790b55537ab984
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:36:39 -0800

    Added: Initial work on the BaseMarketing class #9

---
+++ src/Whiskey-Tycoon.lib/Marketing/Base/BaseMarketing.cs
@@ -0,0 +1,13 @@
+﻿namespace Whiskey_Tycoon.lib.Marketing.Base
+{
+    public abstract class BaseMarketing
+    {
+        public abstract string Name { get; }
+
+        public abstract uint Impact { get; }
+
+        public abstract uint QuartersLength { get; }
+
+        public abstract ulong QuarterCost { get; }
+    }
+}
