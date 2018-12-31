e4ff60ac88c8a37b8f35ccfd896e83162fe10f4e
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 08:19:16 -0800

    Added: Initial work on the ReleaseManagementPageViewModel class

---
+++ src/Whiskey-Tycoon.UWP/ViewModels/ReleaseManagementPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Whiskey_Tycoon.lib.JSONObjects;
+
+namespace Whiskey_Tycoon.UWP.ViewModels
+{
+    public class ReleaseManagementPageViewModel : BaseViewModel
+    {
+        public ReleaseManagementPageViewModel(GameObject game)
+        {
+            Game = game;
+        }
+    }
+}
