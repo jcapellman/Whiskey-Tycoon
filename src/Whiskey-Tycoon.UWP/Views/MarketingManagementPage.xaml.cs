a363c6e219adfd90848914c27c6d5adb2fe6ba0e
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:27:11 -0800

    Added: Initial work on the MarketingManagementPage class #9

---
+++ src/Whiskey-Tycoon.UWP/Views/MarketingManagementPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
+
+using Whiskey_Tycoon.lib.JSONObjects;
+using Whiskey_Tycoon.UWP.ViewModels;
+
+namespace Whiskey_Tycoon.UWP.Views
+{
+    public sealed partial class MarketingManagementPage : BasePage
+    {
+        private MarketingManagementPageViewModel ViewModel => (MarketingManagementPageViewModel)DataContext;
+
+        public MarketingManagementPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (!(e.Parameter is GameObject))
+            {
+                return;
+            }
+
+            DataContext = new MarketingManagementPageViewModel((GameObject)e.Parameter);
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainGamePage), ViewModel.Game);
+        }
+
+        private void btnBuyMarketing_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.AddMarketing();
+
+            ShowMessage($"{ViewModel.SelectedMarketing.Name} obtained!");
+
+            Frame.Navigate(typeof(MainGamePage), ViewModel.Game);
+        }
+    }
+}
