8a5bd5db33db07789dce91638734baa96c27f220
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 08:43:55 -0800

    Added: Initial work on the LoanManagementPage #13

---
+++ src/Whiskey-Tycoon.UWP/Views/LoanManagementPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
+
+using Whiskey_Tycoon.lib.JSONObjects;
+using Whiskey_Tycoon.UWP.ViewModels;
+
+namespace Whiskey_Tycoon.UWP.Views
+{
+    public sealed partial class LoanManagementPage : BasePage
+    {
+        private LoanManagementPageViewModel ViewModel => (LoanManagementPageViewModel)DataContext;
+
+        public LoanManagementPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (!(e.Parameter is GameObject))
+            {
+                return;
+            }
+
+            DataContext = new LoanManagementPageViewModel((GameObject)e.Parameter);
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainGamePage), ViewModel.Game);
+        }
+    }
+}
