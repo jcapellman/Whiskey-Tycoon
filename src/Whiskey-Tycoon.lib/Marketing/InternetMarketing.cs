e299d40923d5cfce53e05a4e3e72b5f5c4110a50
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:51:23 -0800

    Added: Initial work on the InternetMarketing class #9

---
+++ src/Whiskey-Tycoon.lib/Marketing/InternetMarketing.cs
@@ -0,0 +1,15 @@
+﻿using Whiskey_Tycoon.lib.Marketing.Base;
+
+namespace Whiskey_Tycoon.lib.Marketing
+{
+    public class InternetMarketing : BaseMarketing
+    {
+        public override string Name => "Internet";
+
+        public override uint Impact => 15;
+
+        public override uint QuartersLength => 4;
+
+        public override ulong QuarterCost => 50000;
+    }
+}
