7ded75ee8770b2c27b8975d32d305144c823af00
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 08:30:31 -0800

    Added: Viewing of Release information #10

--- src/Whiskey-Tycoon.UWP/ViewModels/ReleaseManagementPageViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/ReleaseManagementPageViewModel.cs
@@ -1,12 +1,30 @@
-﻿using Whiskey_Tycoon.lib.JSONObjects;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+using Whiskey_Tycoon.lib.JSONObjects;
 
 namespace Whiskey_Tycoon.UWP.ViewModels
 {
     public class ReleaseManagementPageViewModel : BaseViewModel
     {
+        private ObservableCollection<ReleasesObject> _releases;
+
+        public ObservableCollection<ReleasesObject> Releases
+        {
+            get => _releases;
+
+            set
+            {
+                _releases = value;
+                OnPropertyChanged();
+            }
+        }
+
         public ReleaseManagementPageViewModel(GameObject game)
         {
             Game = game;
+
+            Releases = new ObservableCollection<ReleasesObject>(Game.Releases.Where(a => !a.Archived).OrderByDescending(a => a.ReleaseYear));
         }
     }
 }
