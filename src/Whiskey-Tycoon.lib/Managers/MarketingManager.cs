9280a99dd3ec6ee1c1d16c9ced147e2271407629
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:36:14 -0800

    Added: Initial work on the MarketingManager class #9

---
+++ src/Whiskey-Tycoon.lib/Managers/MarketingManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+
+using Whiskey_Tycoon.lib.Marketing.Base;
+
+namespace Whiskey_Tycoon.lib.Managers
+{
+    public class MarketingManager
+    {
+        public static ObservableCollection<BaseMarketing> GetMarketingTypes() =>
+            new ObservableCollection<BaseMarketing>(Assembly.GetAssembly(typeof(BaseMarketing)).GetTypes()
+                .Where(a => a.BaseType == typeof(BaseMarketing) && !a.IsAbstract)
+                .Select(b => (BaseMarketing)Activator.CreateInstance(b)).OrderBy(a => a.Impact));
+    }
+}
