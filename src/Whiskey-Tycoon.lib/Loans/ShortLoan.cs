637482c8ac520b2ecf3a8f65b15b0a4df671b142
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:03:40 -0800

    Added: Initial work on the ShortLoan class #13

---
+++ src/Whiskey-Tycoon.lib/Loans/ShortLoan.cs
@@ -0,0 +1,15 @@
+﻿using Whiskey_Tycoon.lib.Loans.Base;
+
+namespace Whiskey_Tycoon.lib.Loans
+{
+    public class ShortLoan : BaseLoan
+    {
+        public override string Name => "Short Loan";
+
+        public override ulong Amount => 100000;
+
+        public override uint QuartersToPayOff => 8;
+
+        public override ulong QuarterlyInterest => 10000;
+    }
+}
