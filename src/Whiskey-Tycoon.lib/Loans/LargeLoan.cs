a449dd2e4132354cb8f4d3ee269500eb1e22ba95
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:02:38 -0800

    Added: Initial work on the LargeLoan class #13

---
+++ src/Whiskey-Tycoon.lib/Loans/LargeLoan.cs
@@ -0,0 +1,15 @@
+﻿using Whiskey_Tycoon.lib.Loans.Base;
+
+namespace Whiskey_Tycoon.lib.Loans
+{
+    public class LargeLoan : BaseLoan
+    {
+        public override string Name => "Large Loan";
+
+        public override ulong Amount => 1000000;
+
+        public override uint QuartersToPayOff => 24;
+
+        public override ulong QuarterlyInterest => 125000;
+    }
+}
