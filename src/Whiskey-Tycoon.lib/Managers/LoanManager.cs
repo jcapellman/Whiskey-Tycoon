c8593373b5c9a92ae65b6556fe7186f38a6ffc22
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:04:36 -0800

    Added: Initial work on the LoanManager class #13

---
+++ src/Whiskey-Tycoon.lib/Managers/LoanManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+
+using Whiskey_Tycoon.lib.Loans.Base;
+
+namespace Whiskey_Tycoon.lib.Managers
+{
+    public class LoanManager
+    {
+        public static ObservableCollection<BaseLoan> GetLoanTypes() =>
+            new ObservableCollection<BaseLoan>(Assembly.GetAssembly(typeof(BaseLoan)).GetTypes()
+                .Where(a => a.BaseType == typeof(BaseLoan) && !a.IsAbstract)
+                .Select(b => (BaseLoan) Activator.CreateInstance(b)).OrderBy(a => a.Amount));
+    }
+}
