3db524e907b745d746cbcfaf222309b8ff0bbcb4
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 08:44:26 -0800

    Added: Initial work on the LoanManagementPageViewModel class #13

---
+++ src/Whiskey-Tycoon.UWP/ViewModels/LoanManagementPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Whiskey_Tycoon.lib.JSONObjects;
+
+namespace Whiskey_Tycoon.UWP.ViewModels
+{
+    public class LoanManagementPageViewModel : BaseViewModel
+    {
+        public LoanManagementPageViewModel(GameObject game)
+        {
+            Game = game;
+        }
+    }
+}
