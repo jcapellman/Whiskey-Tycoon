a7dbaef7d483ad512c4bb6035c6d119ab1698038
Jarred Capellman <jcapellman@hotmail.com>
2018-12-30 20:56:02 -0800

    Added: Initial work on the EventManager class #21

---
+++ src/Whiskey-Tycoon.lib/Managers/EventManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Whiskey_Tycoon.lib.JSONObjects;
+
+namespace Whiskey_Tycoon.lib.Managers
+{
+    public class EventManager
+    {
+        private readonly List<string> _events = new List<string>();
+
+        public void AddEvent(string eventString) => _events.Add(eventString);
+
+        public EventObject GenerateEvent(int currentYear, int currentQuarter)
+        {
+            var eventObject = new EventObject
+            {
+                EventText = string.Join(Environment.NewLine, _events),
+                Year = currentYear,
+                Quarter = currentQuarter
+            };
+
+            _events.Clear();
+
+            return eventObject;
+        }
+    }
+}
