f93cd8ae1d5d65851d03691817a4c2365a212df2
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 10:25:23 -0800

    Added: Initial work on the LoanObject class

---
+++ src/Whiskey-Tycoon.lib/JSONObjects/LoanObject.cs
@@ -0,0 +1,19 @@
+﻿namespace Whiskey_Tycoon.lib.JSONObjects
+{
+    public class LoanObject
+    {
+        public ulong LoanedAmount { get; set; }
+
+        public ulong LoanAmountRemaining { get; set; }
+
+        public ulong QuarterlyInterest { get; set; }
+
+        public ulong QuarterlyPayment { get; set; }
+        
+        public ulong TotalInterest { get; set; }
+
+        public ulong QuartersRemaining { get; set; }
+
+        public ulong PayOffAmount => LoanAmountRemaining + TotalInterest;
+    }
+}
