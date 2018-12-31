8e28ae2089124fd3dd01b396d16578269801f3a6
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 15:35:51 -0800

    Added: Initial work on the MarketingObject class #9

---
+++ src/Whiskey-Tycoon.lib/JSONObjects/MarketingObject.cs
@@ -0,0 +1,11 @@
+﻿namespace Whiskey_Tycoon.lib.JSONObjects
+{
+    public class MarketingObject
+    {
+        public uint Impact { get; set; }
+
+        public ulong QuarterlyCost { get; set; }
+
+        public uint QuartersRemaining { get; set; }
+    }
+}
