77b22d5bcbc67984c03390f25b104dd54567a861
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 19:12:48 -0800

    Added: Initial work on the HighScorePageViewModel class #24

---
+++ src/Whiskey-Tycoon.UWP/ViewModels/HighScorePageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Whiskey_Tycoon.lib.Common;
+using Whiskey_Tycoon.lib.JSONObjects;
+
+namespace Whiskey_Tycoon.UWP.ViewModels
+{
+    public class HighScorePageViewModel : BaseViewModel
+    {
+        private List<HighScoresObject> _highScores;
+
+        public List<HighScoresObject> HighScores
+        {
+            get => _highScores;
+
+            set
+            {
+                _highScores = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public HighScorePageViewModel()
+        {
+            LoadHighScores();
+        }
+
+        private async void LoadHighScores()
+        {
+            var highScoresResult = await App.FileSystem.GetFileAsync<List<HighScoresObject>>(Constants.FILENAME_HIGHSCORES);
+
+            HighScores = highScoresResult?.OrderByDescending(a => a.BottlesSold).ToList() ?? new List<HighScoresObject>();
+        }
+    }
+}
