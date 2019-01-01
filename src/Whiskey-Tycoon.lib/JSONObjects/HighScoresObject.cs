14c9bc52e6faed232e833f3e55acb808df85ff5e
Jarred Capellman <jcapellman@hotmail.com>
2018-12-31 19:13:49 -0800

    Added: Initial work on the HighScoresObject class #24

---
+++ src/Whiskey-Tycoon.lib/JSONObjects/HighScoresObject.cs
@@ -0,0 +1,13 @@
+﻿namespace Whiskey_Tycoon.lib.JSONObjects
+{
+    public class HighScoresObject
+    {
+        public string DistillerName { get; set; }
+
+        public string DistilleryName { get; set; }
+
+        public ulong BottlesSold { get; set; }
+
+        public uint YearsSurvived { get; set; }
+    }
+}
