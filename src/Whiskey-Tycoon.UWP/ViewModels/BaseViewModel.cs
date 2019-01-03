eb9139aa1e8c089f226771de357b0dcac924f3c7
Jarred Capellman <jcapellman@hotmail.com>
2019-01-02 19:25:03 -0800

    Added: EmptyListViewVisibility and ListViewVisibility properties to the BaseViewModel class #25

--- src/Whiskey-Tycoon.UWP/ViewModels/BaseViewModel.cs
+++ src/Whiskey-Tycoon.UWP/ViewModels/BaseViewModel.cs
@@ -1,22 +1,61 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
+using Windows.UI.Xaml;
+
 using Whiskey_Tycoon.lib.JSONObjects;
 
 namespace Whiskey_Tycoon.UWP.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
+        private Visibility _emptyListViewVisibility;
+
+        public Visibility EmptyListViewVisibility
+        {
+            get => _emptyListViewVisibility;
+
+            set
+            {
+                _emptyListViewVisibility = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private Visibility _listViewVisibility;
+
+        public Visibility ListViewVisibility
+        {
+            get => _listViewVisibility;
+
+            set
+            {
+                _listViewVisibility = value;
+
+                OnPropertyChanged();
+
+                EmptyListViewVisibility = value == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            }
+        }
+
         private GameObject _gameObject;
 
         public GameObject Game
         {
-            get { return _gameObject; }
+            get => _gameObject;
+
             set
             {
                 _gameObject = value;
                 OnPropertyChanged();
             }
+        }
+
+        public BaseViewModel()
+        {
+            EmptyListViewVisibility = Visibility.Collapsed;
+            ListViewVisibility = Visibility.Collapsed;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
