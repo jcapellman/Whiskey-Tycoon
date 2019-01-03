9207f1de4f16a818c0e5a1db33ab78ae17c36ed9
Jarred Capellman <jcapellman@hotmail.com>
2019-01-02 18:52:38 -0800

    Added: Ranking property and Constructor to the HighScoresObject class #25

--- src/Whiskey-Tycoon.lib/JSONObjects/HighScoresObject.cs
+++ src/Whiskey-Tycoon.lib/JSONObjects/HighScoresObject.cs
@@ -2,6 +2,8 @@
 {
     public class HighScoresObject
     {
+        public int Ranking { get; set; }
+
         public string DistillerName { get; set; }
 
         public string DistilleryName { get; set; }
@@ -9,5 +11,16 @@
         public ulong BottlesSold { get; set; }
 
         public uint YearsSurvived { get; set; }
+
+        public HighScoresObject() { }
+
+        public HighScoresObject(HighScoresObject highScores, int ranking)
+        {
+            DistillerName = highScores.DistillerName;
+            DistilleryName = highScores.DistilleryName;
+            BottlesSold = highScores.BottlesSold;
+            YearsSurvived = highScores.YearsSurvived;
+            Ranking = ranking;
+        }
     }
 }
