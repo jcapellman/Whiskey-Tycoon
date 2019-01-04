76f0a162f8c6ff026e585ca9fba2fd457c3b8cfb
Jarred Capellman <jcapellman@hotmail.com>
2019-01-03 19:10:08 -0600

    Added: VersionString to the UI #42

--- src/Whiskey-Tycoon.UWP/MainPage.xaml.cs
+++ src/Whiskey-Tycoon.UWP/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
+using Whiskey_Tycoon.UWP.ViewModels;
 using Whiskey_Tycoon.UWP.Views;
 
 namespace Whiskey_Tycoon.UWP
@@ -10,6 +11,8 @@ namespace Whiskey_Tycoon.UWP
         public MainPage()
         {
             InitializeComponent();
+
+            DataContext = new MainPageViewModel();
         }
 
         private void btnNewGame_Click(object sender, RoutedEventArgs e)
