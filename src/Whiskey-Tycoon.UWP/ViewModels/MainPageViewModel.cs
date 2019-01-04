adf3918b27be3e28f29e715cf4acf0dfc934951b
Jarred Capellman <jcapellman@hotmail.com>
2019-01-03 19:09:40 -0600

    Added: Initial work on the MainGamePageViewModel class #42

---
+++ src/Whiskey-Tycoon.UWP/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Windows.ApplicationModel;
+
+namespace Whiskey_Tycoon.UWP.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        public MainPageViewModel()
+        {
+            VersionString =
+                $"Version {Package.Current.Id.Version.Major}.{Package.Current.Id.Version.Minor}.{Package.Current.Id.Version.Build}.{Package.Current.Id.Version.Revision}";
+        }
+
+        private string _versionString;
+
+        public string VersionString
+        {
+            get => _versionString;
+
+            set
+            {
+                _versionString = value;
+
+                OnPropertyChanged();
+            }
+        }
+    }
+}
